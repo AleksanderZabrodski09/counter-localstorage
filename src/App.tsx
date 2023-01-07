27f1efc9b7089e13677956bb93f518a1e471f4bb
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-01-07 21:03:51 +0100

    create: add useEffect and change useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
 
-  const [value, setValue] = useState<number>(0)
+  const [value, setValue] = useState<number>(() => {
+    let valueAsString = localStorage.getItem('countValue')
+    if (valueAsString) {
+      let newValue = JSON.parse(valueAsString)
+      return newValue
+    } else return 0
+  })
 
   const onClickHandler = () => {
     setValue(value + 1)
   }
-  const setLocalStorageHandler = () => {
-    // localStorage.setItem('countValue',value.toString())
-    localStorage.setItem('countValue',JSON.stringify(value))
-  }
 
-  const getFromLocalStorageHandler = () => {
-    //code
-  }
+  useEffect(() => {
+    localStorage.setItem('countValue', JSON.stringify(value))
+  }, [value])
+
 
   return (
     <div className="App">
       <h1>{value}</h1>
       <button onClick={onClickHandler}>inc</button>
-      <button onClick={setLocalStorageHandler}>setLocalStorage</button>
-      <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+
     </div>
   );
 }
