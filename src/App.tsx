aae67912ddc9e7ba5f1b8b777af6ee2eb545684b
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-01-08 14:25:41 +0100

    create: add useSelector, useDispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppRootReducerType} from './bll/store';
+import {insCounterAC} from './bll/counter-reducer';
 
 function App() {
 
-  const [value, setValue] = useState<number>(() => {
-    let valueAsString = localStorage.getItem('countValue')
-    if (valueAsString) {
-      let newValue = JSON.parse(valueAsString)
-      return newValue
-    } else return 0
-  })
-
+  const value=useSelector<AppRootReducerType, number>(state => state.counter.value)
+  const dispatch = useDispatch()
   const onClickHandler = () => {
-    setValue(value + 1)
+    dispatch(insCounterAC())
   }
 
-  useEffect(() => {
-    localStorage.setItem('countValue', JSON.stringify(value))
-  }, [value])
+
+  // const [value, setValue] = useState<number>(() => {
+  //   let valueAsString = localStorage.getItem('countValue')
+  //   if (valueAsString) {
+  //     let newValue = JSON.parse(valueAsString)
+  //     return newValue
+  //   } else return 0
+  // })
+  //
+
+  //
+  // useEffect(() => {
+  //   localStorage.setItem('countValue', JSON.stringify(value))
+  // }, [value])
 
 
   return (
--- src/bll/store.ts
+++ src/bll/store.ts
@@ -2,9 +2,10 @@ import {combineReducers, legacy_createStore} from 'redux';
 import {counterReducer} from './counter-reducer';
 
 
-
+export type AppRootReducerType = ReturnType<typeof rootReducer>
 const rootReducer = combineReducers({
   counter : counterReducer
 })
 
 export const store = legacy_createStore(rootReducer)
+type StoreType = typeof store
