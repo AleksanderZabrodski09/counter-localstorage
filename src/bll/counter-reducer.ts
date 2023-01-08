d3fc73cb7fa39934f49ea1d4ae0871d8d7120981
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-01-08 14:26:04 +0100

    create: add insCounterAC, setValueFromLocalStorageAC

--- src/bll/counter-reducer.ts
+++ src/bll/counter-reducer.ts
@@ -1,16 +1,22 @@
-
-
-
-
-type InitialStateType= typeof initialState
-const initialState={
-  value:0,
+export type InitialStateType = typeof initialState
+const initialState = {
+  value: 0,
 }
 
-export const counterReducer =(state:InitialStateType = initialState, action: any)=>{
-  switch(action.type){
+export const counterReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
+  switch (action.type) {
+    case 'INS-COUNTER':
+      return {...state, value: state.value +1}
+    case 'SET-VALUE-FROM-LS':
+      return {...state, value: action.value}
     default:
       return state
+  }
 }
 
-}
+export const insCounterAC = ()=>({type:'INS-COUNTER'}as const)
+export const setValueFromLocalStorageAC = (value:number)=>({type:'SET-VALUE-FROM-LS', value}as const)
+
+type ActionType =
+  | ReturnType<typeof insCounterAC>
+  | ReturnType<typeof setValueFromLocalStorageAC>
