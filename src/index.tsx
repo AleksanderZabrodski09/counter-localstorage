3014790a254a99b63411b2001ef3a908483fd893
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-01-08 14:19:56 +0100

    create: add Provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {store} from './bll/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(
+  <Provider store={store}><App/></Provider>
+);
